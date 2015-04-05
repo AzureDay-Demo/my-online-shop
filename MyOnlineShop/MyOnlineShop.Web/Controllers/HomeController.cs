84aec15f80a009758b03b25a88c8e2a66da55abe
Boyko Anton <boyko.ant@live.com>
2015-04-05 18:36:56 +0300

    init

---
+++ MyOnlineShop/MyOnlineShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using MyOnlineShop.Web.Context;
+using MyOnlineShop.Web.Models;
+
+namespace MyOnlineShop.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var model = new CategoryListModel();
+
+			using (var context = new MyOnlineShopContext())
+			{
+				model.Categories = context
+					.Categories
+					.Include(i => i.Goods)
+					.ToList();
+			}
+
+			return View(model);
+		}
+	}
+}
